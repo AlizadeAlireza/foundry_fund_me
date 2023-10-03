1e28d9b8d73dd50a3f94c6a7973e70ddee5781c0
alireza alizadeh <alireza.aa9700@gmail.com>
2023-10-03 14:13:52 +0330

    funded amount success and adding the user and money for test

--- test/FundMeTest.t.sol
+++ test/FundMeTest.t.sol
@@ -8,6 +8,11 @@ import {DeployFundMe} from "../script/DeployFundMe.s.sol";
 
 contract FundMeTest is Test {
     FundMe fundMe;
+
+    address USER = makeAddr("user");
+    uint constant SEND_VALUE = 0.1 ether; // 100.000.000.000.000.000
+    uint constant STARTING_BALANCE = 10 ether;
+
     address sepolia_priceFeed = 0x694AA1769357215DE4FAC081bf1f309aDC325306;
 
     function setUp() external {
@@ -15,6 +20,7 @@ contract FundMeTest is Test {
         // new deploy contract
         DeployFundMe deployFundMe = new DeployFundMe();
         fundMe = deployFundMe.run();
+        vm.deal(USER, STARTING_BALANCE);
     }
 
     function testMinimumDollarIsFive() public {
@@ -41,5 +47,14 @@ contract FundMeTest is Test {
     function testFundFailsWithoutEnoughEth() public {
         vm.expectRevert();
         fundMe.fund();
+    }
+
+    function testFundUpdatesFundedDataStructure() public {
+        // prank is for creating a signer
+        vm.prank(USER); // the next tx will be send by the user
+        fundMe.fund{value: SEND_VALUE}();
+
+        uint256 amountFunded = fundMe.getAddressToAmountFunded(USER);
+        assertEq(amountFunded, SEND_VALUE);
     }
 }
