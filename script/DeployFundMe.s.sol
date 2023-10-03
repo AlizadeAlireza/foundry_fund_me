ccb8125b8810a27beb9d7d32e452f0e4484c2171
alireza alizadeh <alireza.aa9700@gmail.com>
2023-10-03 09:42:24 +0330

    change test setUp with deploying script

--- script/DeployFundMe.s.sol
+++ script/DeployFundMe.s.sol
@@ -2,17 +2,13 @@
 pragma solidity ^0.8.19;
 
 import {Script} from "forge-std/Script.sol";
-import {HelperConfig} from "./HelperConfig.s.sol";
 import {FundMe} from "../src/FundMe.sol";
 
 contract DeployFundMe is Script {
-    function run() external returns (FundMe, HelperConfig) {
-        HelperConfig helperConfig = new HelperConfig(); // This comes with our mocks!
-        address priceFeed = helperConfig.activeNetworkConfig();
-
+    function run() external returns (FundMe) {
         vm.startBroadcast();
-        FundMe fundMe = new FundMe(priceFeed);
+        FundMe fundMe = new FundMe(0x694AA1769357215DE4FAC081bf1f309aDC325306);
         vm.stopBroadcast();
-        return (fundMe, helperConfig);
+        return (fundMe);
     }
 }
--- test/FundMeTest.t.sol
+++ test/FundMeTest.t.sol
@@ -4,13 +4,17 @@ pragma solidity ^0.8.19;
 
 import {Test, console} from "forge-std/Test.sol";
 import {FundMe} from "../src/FundMe.sol";
+import {DeployFundMe} from "../script/DeployFundMe.s.sol";
 
 contract FundMeTest is Test {
     FundMe fundMe;
     address sepolia_priceFeed = 0x694AA1769357215DE4FAC081bf1f309aDC325306;
 
     function setUp() external {
-        fundMe = new FundMe(sepolia_priceFeed);
+        // fundMe = new FundMe(sepolia_priceFeed);
+        // new deploy contract
+        DeployFundMe deployFundMe = new DeployFundMe();
+        fundMe = deployFundMe.run();
     }
 
     function testMinimumDollarIsFive() public {
