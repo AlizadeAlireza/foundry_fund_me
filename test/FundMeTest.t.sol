276f64489cd664469cd55ddd94b258e5cb2f2205
alireza alizadeh <alireza.aa9700@gmail.com>
2023-10-03 14:42:25 +0330

    after funding and withdrawing

--- test/FundMeTest.t.sol
+++ test/FundMeTest.t.sol
@@ -77,4 +77,24 @@ contract FundMeTest is Test {
         vm.expectRevert();
         fundMe.withdraw();
     }
+
+    function testWithdrawWithASingleFunder() public funded {
+        // arrange
+        uint startingOwnerBalance = fundMe.getOwner().balance;
+        uint startingFundMeBalance = address(fundMe).balance;
+
+        // act
+        vm.prank(fundMe.getOwner());
+        fundMe.withdraw();
+
+        // assert
+        uint endingOwnerBalance = fundMe.getOwner().balance;
+        uint endingFundMeBalance = address(fundMe).balance;
+
+        assertEq(endingFundMeBalance, 0);
+        assertEq(
+            startingFundMeBalance + startingOwnerBalance,
+            endingOwnerBalance
+        );
+    }
 }
