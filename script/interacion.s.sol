a7ba4276c3af0bbc86bcbf4bca77c182848d6820
alireza alizadeh <alireza.aa9700@gmail.com>
2023-10-03 18:32:40 +0330

    initial interaction script

---
+++ script/interacion.s.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.19;
+
+import {Script, console} from "forge-std/Script.sol";
+
+contract FundFundMe is Script {}
+
+contract WithdrawFundMe is Script {}
