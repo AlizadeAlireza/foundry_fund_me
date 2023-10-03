cbc0c74e26c209c627d014f437cb46f7e362d240
alireza alizadeh <alireza.aa9700@gmail.com>
2023-10-03 15:00:30 +0330

    testWithdrawFromMultipleFunders

--- test/FundMeTest.t.sol
+++ test/FundMeTest.t.sol
@@ -97,4 +97,37 @@ contract FundMeTest is Test {
             endingOwnerBalance
         );
     }
+
+    function testWithdrawFromMultipleFunders() public funded {
+        // arrange
+
+        // assert
+        uint160 numberOfFunders = 10;
+        uint160 startingFunderIndex = 2;
+
+        for (
+            uint160 i = startingFunderIndex;
+            i < numberOfFunders + startingFunderIndex;
+            i++
+        ) {
+            // we get hoax from stdcheats
+            // prank + deal
+            hoax(address(i), STARTING_BALANCE);
+            fundMe.fund{value: SEND_VALUE}();
+        }
+        uint startingOwnerBalance = fundMe.getOwner().balance;
+        uint startingFundMeBalance = address(fundMe).balance;
+
+        // act
+        vm.startPrank(fundMe.getOwner());
+        fundMe.withdraw();
+        vm.stopPrank();
+
+        // assert
+        assert(address(fundMe).balance == 0);
+        assert(
+            startingFundMeBalance + startingOwnerBalance ==
+                fundMe.getOwner().balance
+        );
+    }
 }
