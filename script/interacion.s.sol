708806807c3e5c41228f150d9d4db04b84e13e8f
alireza alizadeh <alireza.aa9700@gmail.com>
2023-10-03 18:47:54 +0330

    fundme script

--- script/interacion.s.sol
+++ script/interacion.s.sol
@@ -3,8 +3,19 @@ pragma solidity ^0.8.19;
 
 import {Script, console} from "forge-std/Script.sol";
 import {DevOpsTools} from "foundry-devops/src/DevOpsTools.sol";
+import {FundMe} from "../src/FundMe.sol";
 
 contract FundFundMe is Script {
+    uint256 SEND_VALUE = 0.1 ether;
+
+    function fundFundMe(address mostRecentlyDeployed) public {
+        vm.startBroadcast();
+        // wrap the recently deployed
+        FundMe(payable(mostRecentlyDeployed)).fund{value: SEND_VALUE}();
+        vm.stopBroadcast();
+        console.log("Funded FundMe with %s", SEND_VALUE);
+    }
+
     function run() external {
         address mostRecentlyDeployed = DevOpsTools.get_most_recent_deployment(
             "FundMe",
