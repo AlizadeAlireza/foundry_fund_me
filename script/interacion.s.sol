637d4aa00af56abc0861220699cb02bf92d5af59
alireza alizadeh <alireza.aa9700@gmail.com>
2023-10-03 18:42:20 +0330

    most recent contract --> like [-1] in script languages

--- script/interacion.s.sol
+++ script/interacion.s.sol
@@ -2,7 +2,16 @@
 pragma solidity ^0.8.19;
 
 import {Script, console} from "forge-std/Script.sol";
+import {DevOpsTools} from "foundry-devops/src/DevOpsTools.sol";
 
-contract FundFundMe is Script {}
+contract FundFundMe is Script {
+    function run() external {
+        address mostRecentlyDeployed = DevOpsTools.get_most_recent_deployment(
+            "FundMe",
+            block.chainid
+        );
+        fundFundMe(mostRecentlyDeployed);
+    }
+}
 
 contract WithdrawFundMe is Script {}
