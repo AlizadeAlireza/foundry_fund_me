bcd1b261beab21c536ac717ce1546c6cd81f23ec
alireza alizadeh <alireza.aa9700@gmail.com>
2023-10-03 10:33:11 +0330

    add ethUsdPrice feed to the deploy contract

--- script/DeployFundMe.s.sol
+++ script/DeployFundMe.s.sol
@@ -7,8 +7,13 @@ import {HelperConfig} from "../script/HelperConfig.s.sol";
 
 contract DeployFundMe is Script {
     function run() external returns (FundMe) {
+        // before broadcast  is not a real tx
+        HelperConfig helperConfig = new HelperConfig();
+        address ethUsdPriceFeed = helperConfig.activeNetworkConfig();
+
+        // after broadcast is a real tx
         vm.startBroadcast();
-        FundMe fundMe = new FundMe(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+        FundMe fundMe = new FundMe(ethUsdPriceFeed);
         vm.stopBroadcast();
         return (fundMe);
     }
